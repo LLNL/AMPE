7e89da4b19e8d0a7b16112f1f0dd8f3d07794fe2
Jean-Luc Fattebert <fattebertj@ornl.gov>
2018-01-19 17:10:35 -0500

    Adde test for computation of determinant of 4x4 matrix using general formula

--- source/testMath.C
+++ source/testMath.C
@@ -8,13 +8,13 @@ using namespace std;
 
 int main( int argc, char *argv[] )
 {
-   std::string input_filename;
-   input_filename = argv[1];
+   cout<<"Test determinant computation."<<endl;
 
    double* mat[4];
    double work[16]={11,2,3,4,5,16,7,8,9,10,11,12,13,14,15,16};
    for(short i=0;i<4;++i)mat[i]=&work[4*i];
 
+   cout<<"Test function Determinant4..."<<endl;
    double d=Determinant4(mat); 
 
    const double tol=1.e-8;
@@ -25,6 +25,16 @@ int main( int argc, char *argv[] )
       cout<<"TEST successful!"<<endl;
    }
    
+   cout<<"Test function DeterminantN..."<<endl;
+   d=DeterminantN(mat,4);
+   if( fabs(d+400.)>tol )
+   {
+      cerr<<"TEST: Determinant of 4x4 matrix failed!!!"<<endl;
+      cerr<<"computed d="<<d<<endl;
+      cerr<<"exact d="<<400.<<endl;
+   }else{
+      cout<<"TEST successful!"<<endl;
+   }
 
    return(0);
 }
