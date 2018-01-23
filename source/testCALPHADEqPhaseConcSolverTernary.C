27b66246ca613ffb0b102c589ec9b2821d415ec8
Jean-Luc Fattebert <fattebertj@ornl.gov>
2018-01-23 15:00:55 -0500

    Added testing of RHS and Jacobian of CALPHADConcSolverTernary in testCALPHADEqPhaseConcSolverTernary

--- source/testCALPHADEqPhaseConcSolverTernary.C
+++ source/testCALPHADEqPhaseConcSolverTernary.C
@@ -1,4 +1,5 @@
 #include "CALPHADEqPhaseConcSolverTernary.h"
+#include "CALPHADConcSolverTernary.h"
 
 #include <iostream>
 #include <iomanip>
@@ -70,6 +71,46 @@ int main( int argc, char *argv[] )
       for(int i=0;i<5;i++)fd[i]=(fvec2[i]-fvec1[i])/epsilon;
 
       for(int i=0;i<5;i++)
+      {
+         if( fabs(fd[i]-fjac[i][j])>tol )
+         {
+            cerr<<"ERROR: Equation "<<i<<", FD="<<fd[i]<<", fjac="<<fjac[i][j]<<endl;
+         }
+      }
+
+      x[j]-=epsilon;
+   }
+
+   cout<<"====================================="<<endl;
+   cout<<"Test CALPHADConcentrationSolverTernary"<<endl;
+   CALPHADConcentrationSolverTernary solver2;
+
+   solver2.setup(cA,cB,0.5,
+                RTinv,
+                L_AB_L,L_AC_L,L_BC_L,
+                L_AB_S,L_AC_S,L_BC_S,
+                L_ABC_L,L_ABC_S,
+                fA,fB,fC);
+
+   solver2.RHS(x,fvec1);
+
+   solver2.Jacobian(x,fjac);
+
+   cerr<<setprecision(12);
+
+   //loop over variables
+   for(int j=0;j<4;j++)
+   {
+      cout<<"----------------------------"<<endl;
+      cout<<"Test variations of variable "<<j<<endl;
+      x[j]+=epsilon;
+      solver2.RHS(x,fvec2);
+
+      double fd[4];
+      //loop over equations
+      for(int i=0;i<4;i++)fd[i]=(fvec2[i]-fvec1[i])/epsilon;
+
+      for(int i=0;i<4;i++)
       {
          if( fabs(fd[i]-fjac[i][j])>tol )
          {
