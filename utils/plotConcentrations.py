1cd9f25974e4787ab3b157a2c55f1c157fe939f5
Jean-Luc Fattebert <fattebertj@ornl.gov>
2018-03-29 18:00:52 -0400

    Added example for solidification in ternary alloy (1d geometry)

--- utils/plotConcentrations.py
+++ utils/plotConcentrations.py
@@ -15,5 +15,18 @@ for fname in filenames:
       color=0
 
 #save plot into png file    
-plt.savefig('figure.png')
+plt.savefig('C0.png')
+plt.close()
 
+filenames=sorted(glob.glob('concentration1*.dat'))
+if len(filenames)>0:
+  color=0
+  for fname in filenames:
+    data=np.loadtxt(fname)
+    X=data[:,0]
+    Y=data[:,1]
+    plt.plot(X,Y,colors[color])
+    color=color+1
+    if color>6:
+      color=0
+  plt.savefig('C1.png')
