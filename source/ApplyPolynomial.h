7dede0be74a66efccbdd66c7d29f364b50ff2982
Jean-Luc Fattebert <fattebertj@ornl.gov>
2021-07-29 11:50:12 -0400

    Add class ApplyPolynomial

---
+++ source/ApplyPolynomial.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2018, Lawrence Livermore National Security, LLC and
+// UT-Battelle, LLC.
+// Produced at the Lawrence Livermore National Laboratory and
+// the Oak Ridge National Laboratory
+// Written by M.R. Dorr, J.-L. Fattebert and M.E. Wickett
+// LLNL-CODE-747500
+// All rights reserved.
+// This file is part of AMPE.
+// For details, see https://github.com/LLNL/AMPE
+// Please also read AMPE/LICENSE.
+#ifndef included_ApplyPolynomial
+#define included_ApplyPolynomial
+
+#include "InterpolationType.h"
+
+#include "SAMRAI/hier/PatchHierarchy.h"
+#include "SAMRAI/hier/PatchLevel.h"
+
+using namespace SAMRAI;
+#ifdef HAVE_THERMO4PFM
+using namespace Thermo4PFM;
+#else
+using namespace ampe_thermo;
+#endif
+
+class ApplyPolynomial
+{
+ public:
+   ApplyPolynomial(EnergyInterpolationType interp_func_type);
+
+   void apply(const std::shared_ptr<hier::PatchHierarchy> hierarchy,
+              const int src_cell_data_id, const int dst_cell_data_id);
+
+ private:
+   EnergyInterpolationType d_interp_func_type;
+
+   void apply(const std::shared_ptr<hier::PatchLevel> level,
+              const int src_cell_data_id, const int dst_cell_data_id);
+};
+
+#endif
