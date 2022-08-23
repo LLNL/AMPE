7f913f2e412bb80ab125e64570fba82349091951
Jean-Luc Fattebert <fattebertj@ornl.gov>
2022-08-23 12:58:20 -0400

    Fix FolchPlapp tilting function for phi<0

--- source/TiltingFolchPlapp2005.cc
+++ source/TiltingFolchPlapp2005.cc
@@ -18,6 +18,8 @@
 double TiltingFolchPlapp2005::g0(const double p0, const double p1,
                                  const double p2)
 {
+   if (p0 < 0.) return 0.;
+
    return 0.25 * p0 * p0 *
           (15. * (1. - p0) * (1. + p0 - (p2 - p1) * (p2 - p1)) +
            p0 * (9. * p0 * p0 - 5.));
@@ -28,6 +30,8 @@ double TiltingFolchPlapp2005::g0(const double p0, const double p1,
 double TiltingFolchPlapp2005::dg0dp0(const double p0, const double p1,
                                      const double p2)
 {
+   if (p0 < 0.) return 0.;
+
    return 2.5 * p0 *
           ((p2 - p1) * (p2 - p1) * (3. * p0 - 2.) +
            (1. - p0) * (1. - p0) * (3. * p0 + 2.));
@@ -36,5 +40,7 @@ double TiltingFolchPlapp2005::dg0dp0(const double p0, const double p1,
 double TiltingFolchPlapp2005::dg0dp1(const double p0, const double p1,
                                      const double p2)
 {
+   if (p0 < 0.) return 0.;
+
    return -0.5 * dg0dp0(p1, p0, p2) + 7.5 * (p1 * p1 * (1. - p1) * (p2 - p0));
 }
