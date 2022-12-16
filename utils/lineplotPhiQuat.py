3e6626637064ea23c11248711925ac70b8b0deba
Jean-Luc Fattebert <fattebertj@ornl.gov>
2022-12-16 18:34:35 -0500

    Update lineplotPhiQuat.py to python3 (#116)

--- utils/lineplotPhiQuat.py
+++ utils/lineplotPhiQuat.py
@@ -2,36 +2,11 @@
 # UT-Battelle, LLC.
 # Produced at the Lawrence Livermore National Laboratory and
 # the Oak Ridge National Laboratory
-# Written by M.R. Dorr, J.-L. Fattebert and M.E. Wickett
 # LLNL-CODE-747500
 # All rights reserved.
 # This file is part of AMPE. 
 # For details, see https://github.com/LLNL/AMPE
 # Please also read AMPE/LICENSE.
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
-# - Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the disclaimer below.
-# - Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the disclaimer (as noted below) in the
-#   documentation and/or other materials provided with the distribution.
-# - Neither the name of the LLNS/LLNL nor the names of its contributors may be
-#   used to endorse or promote products derived from this software without
-#   specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
-# LLC, UT BATTELLE, LLC,
-# THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 # 
 #usage:
 #  visit -cli -verbose -nowin -s lineplotPhiQuat.py
@@ -42,14 +17,14 @@ visitdir =os.getcwd()
 filename=visitdir+"/dumps.visit"
 
 myfile=open(filename,'r')
-L=myfile.readlines()
+lines=myfile.readlines()
 
 frames=[]
-for line in L:
-  words=string.split(line)
+for line in lines:
+  words=line.split()
   frames.append(words[0])
 
-print "#",frames
+print("# {}".format(frames))
 
 #load data
 DB=[]
@@ -84,7 +59,7 @@ for db in DB:
       # Do a lineout on variable to produce curve.
       Lineout(p0, p1, ("default"))
     else:
-      print '#remove var ',var,' (not found)...'
+      print("remove var {} (not found)...".format(var))
       vars2remove.append(var)
   for var in vars2remove:
     Vars.remove(var)
@@ -104,13 +79,11 @@ for k in range(len(DB)):
     filename = "%s_%s.dat" % (Vars[j],t0)
     output = open(filename,'w')
     output.write("# %s --- time: %g" % (Vars[j],t0))
-    print "# %s --- time: %g" % (Vars[j],t0)
+    print("# {} --- time: {}".format(Vars[j],t0))
 
-    # Write data as "x  1.-y"
+    # Write data as "x  y"
     for i in range(len(vals) / 2):
-      #print "%g  %g" % (p0[0]+vals[2*i], vals[2*i+1])
       output.write("%g  %g\n" % (p0[0]+vals[2*i], vals[2*i+1]))
-    #print "\n"
     output.write("\n")
 
 sys.exit()
