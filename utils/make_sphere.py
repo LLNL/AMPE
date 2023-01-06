3245a5c507c3426e03eab04923912449a2185b5b
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-01-06 15:23:56 -0500

    Stop using scipy.io for netcdf (#122)

--- utils/make_sphere.py
+++ utils/make_sphere.py
@@ -2,36 +2,11 @@
 # UT-Battelle, LLC.
 # Produced at the Lawrence Livermore National Laboratory and
 # the Oak Ridge National Laboratory
-# Written by M.R. Dorr, J.-L. Fattebert and M.E. Wickett
 # LLNL-CODE-747500
 # All rights reserved.
 # This file is part of AMPE. 
 # For details, see https://github.com/LLNL/AMPE
 # Please also read AMPE/LICENSE.
-# Redistribution and use in source and binary forms, with or without 
-# modification, are permitted provided that the following conditions are met:
-# - Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the disclaimer below.
-# - Redistributions in binary form must reproduce the above copyright notice,
-#   this list of conditions and the disclaimer (as noted below) in the
-#   documentation and/or other materials provided with the distribution.
-# - Neither the name of the LLNS/LLNL nor the names of its contributors may be
-#   used to endorse or promote products derived from this software without
-#   specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
-# LLC, UT BATTELLE, LLC,
-# THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
 # 
 # standard packages
 import math
@@ -45,8 +20,7 @@ import quat as Q
 
 # other required packages
 import numpy as N
-#from Scientific.IO import NetCDF
-from scipy.io import netcdf as NetCDF
+import netCDF4 as nc4
 
 #-----------------------------------------------------------------------
 # command-line arguments and options
@@ -410,7 +384,7 @@ if ( n_spheres == "random" ) :
 #-----------------------------------------------------------------------
 # Open and define file
 
-f = NetCDF.NetCDFFile( filename, 'w' )
+f = nc4.Dataset(filename, 'w', format='NETCDF4')
 
 f.createDimension( 'x', nx )
 f.createDimension( 'y', ny )
