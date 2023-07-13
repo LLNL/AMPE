023cee5c4baef1d35c114688b4f9bd710a340649
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-07-13 10:12:01 -0400

    Fix noise contribution in initial conditions setup

--- tests/SolidifyQuaternions/test3d.py
+++ tests/SolidifyQuaternions/test3d.py
@@ -47,7 +47,7 @@ for line in lines:
       print(line)
       words=line.split()
       sfraction=eval(words[6])
-      if abs(sfraction-0.33)>1.e-2:
+      if abs(sfraction-0.36)>1.e-2:
         print("Wrong solid fraction")
         sys.exit(1)
 
