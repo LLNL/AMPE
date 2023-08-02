edf2b0134073437b51305aba2423f339e517cfc1
Jean-Luc Fattebert <fattebertj@ornl.gov>
2023-08-01 21:33:58 -0400

    Clean up code related to composition diffusion coefficients
    Remove some unused code and unnecessary virtual functions

--- source/TbasedCompositionDiffusionStrategy.h
+++ source/TbasedCompositionDiffusionStrategy.h
@@ -8,30 +8,6 @@
 // This file is part of AMPE.
 // For details, see https://github.com/LLNL/AMPE
 // Please also read AMPE/LICENSE.
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-// - Redistributions of source code must retain the above copyright notice,
-//   this list of conditions and the disclaimer below.
-// - Redistributions in binary form must reproduce the above copyright notice,
-//   this list of conditions and the disclaimer (as noted below) in the
-//   documentation and/or other materials provided with the distribution.
-// - Neither the name of the LLNS/LLNL nor the names of its contributors may be
-//   used to endorse or promote products derived from this software without
-//   specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL LAWRENCE LIVERMORE NATIONAL SECURITY,
-// LLC, UT BATTELLE, LLC,
-// THE U.S. DEPARTMENT OF ENERGY OR CONTRIBUTORS BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-// DAMAGES  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
-// OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-// HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-// STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
-// IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
 //
 #ifndef TbasedCompositionDiffusionStrategy_H
 #define TbasedCompositionDiffusionStrategy_H
@@ -45,10 +21,9 @@ class TbasedCompositionDiffusionStrategy : public CompositionDiffusionStrategy
  public:
    TbasedCompositionDiffusionStrategy(
        const int pfm_diffusion_l_id, const int pfm_diffusion_a_id,
-       const int pfm_diffusion_b_id, const int diffusion_coeff_l_id,
-       const int diffusion_coeff_a_id, const int diffusion_coeff_b_id,
-       const double D_liquid, const double Q0_liquid, const double D_solid_A,
-       const double Q0_solid_A, const double D_solid_B, const double Q0_solid_B,
+       const int pfm_diffusion_b_id, const double D_liquid,
+       const double Q0_liquid, const double D_solid_A, const double Q0_solid_A,
+       const double D_solid_B, const double Q0_solid_B,
        DiffusionInterpolationType interp_func_type,
        const std::string& avg_func_type);
 
@@ -61,13 +36,6 @@ class TbasedCompositionDiffusionStrategy : public CompositionDiffusionStrategy
        const std::shared_ptr<hier::PatchHierarchy> hierarchy,
        const int temperature_id, const int phase_id, const int eta_id);
 
-   /*
-    * Compute diffusion coefficient in each phase
-    */
-   virtual void setDiffCoeffInEachPhase(
-       const std::shared_ptr<hier::PatchHierarchy> hierarchy,
-       const int temperature_id, const int eta_scratch_id);
-
  private:
    /*!
     * holds data for diffusion coefficients in composition equation
@@ -76,10 +44,6 @@ class TbasedCompositionDiffusionStrategy : public CompositionDiffusionStrategy
    int d_pfm_diffusion_l_id;
    int d_pfm_diffusion_a_id;
    int d_pfm_diffusion_b_id;
-
-   int d_diffusion_coeff_l_id;
-   int d_diffusion_coeff_a_id;
-   int d_diffusion_coeff_b_id;
 
    double d_D_liquid;
    double d_Q0_liquid;
