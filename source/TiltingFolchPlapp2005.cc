65d3e093021e70ab78672ef187e667abca53c137
Jean-Luc Fattebert <fattebertj@ornl.gov>
2024-06-24 14:13:38 -0400

    Fix extension of Folch-Plapp tilting function outside [0,1]

--- source/TiltingFolchPlapp2005.cc
+++ source/TiltingFolchPlapp2005.cc
@@ -18,6 +18,7 @@ double TiltingFolchPlapp2005::g0(const double p0, const double p1,
                                  const double p2)
 {
    if (p0 < 0.) return 0.;
+   if (p0 > 1.) return 1.;
 
    return 0.25 * p0 * p0 *
           (15. * (1. - p0) * (1. + p0 - (p2 - p1) * (p2 - p1)) +
@@ -31,6 +32,9 @@ double TiltingFolchPlapp2005::dg0dp0(const double p0, const double p1,
 {
    if (p0 < 0.) return 0.;
 
+   // p0=1 -> p1=p2=0
+   if (p0 > 1.) return 0.;
+
    return 2.5 * p0 *
           ((p2 - p1) * (p2 - p1) * (3. * p0 - 2.) +
            (1. - p0) * (1. - p0) * (3. * p0 + 2.));
@@ -40,6 +44,10 @@ double TiltingFolchPlapp2005::dg0dp1(const double p0, const double p1,
                                      const double p2)
 {
    if (p0 < 0.) return 0.;
+   if (p0 > 1.) return 0.;
+
+   if (p1 < 0.) return 0.;
+   if (p1 > 1.) return 0.;
 
    return -0.5 * dg0dp0(p1, p0, p2) + 7.5 * (p1 * p1 * (1. - p1) * (p2 - p0));
 }
