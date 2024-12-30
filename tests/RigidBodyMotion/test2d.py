47b8cc1ae3634c09aca52b8301c69e52384ffa19
Jean-Luc Fattebert <fattebertj@ornl.gov>
2024-12-30 11:12:11 -0500

    Enable more tests with existing links

--- tests/RigidBodyMotion/test2d.py
+++ tests/RigidBodyMotion/test2d.py
@@ -13,7 +13,11 @@ datadir = sys.argv[6]
 #make symbolic link to calphad data
 data = "1sphere.csv"
 src = datadir+'/'+data
-os.symlink(src, data)
+try:
+  os.symlink(src, data)
+except FileExistsError:
+  os.remove(data)
+  os.symlink(src, data)
 
 #prepare initial conditions file
 initfilename="1sphere.nc"
