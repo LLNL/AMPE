a3ca81c5122e43c8b7a6f50b419294c4347f1ade
Jean-Luc Fattebert <fattebertj@ornl.gov>
2025-01-07 15:51:15 -0500

    Fix factory

--- source/PhaseConcentrationsStrategyFactory.h
+++ source/PhaseConcentrationsStrategyFactory.h
@@ -245,12 +245,24 @@ class PhaseConcentrationsStrategyFactory
                      tbox::plog << "Quadratic..." << std::endl;
                      assert(conc_b_scratch_id == -1);
                      phase_conc_strategy.reset(
-                         new QuadraticEquilibriumPhaseConcentrationsBinary(
+                         new QuadraticEquilibriumThreePhasesTernaryMultiOrder(
+                             model_parameters.norderpA(), conc_l_scratch_id,
+                             conc_a_scratch_id, conc_b_scratch_id,
+                             model_parameters, conc_db));
+                  } else {
+                     phase_conc_strategy.reset(
+                         new QuadraticEquilibriumPhaseConcentrationsBinaryMultiOrder(
                              conc_l_scratch_id, conc_a_scratch_id,
-                             model_parameters.energy_interp_func_type(),
-                             model_parameters.conc_interp_func_type(),
-                             conc_db));
+                             model_parameters, conc_db));
                   }
+               } else {
+                  tbox::plog << "Quadratic..." << std::endl;
+                  assert(conc_b_scratch_id == -1);
+                  phase_conc_strategy.reset(
+                      new QuadraticEquilibriumPhaseConcentrationsBinary(
+                          conc_l_scratch_id, conc_a_scratch_id,
+                          model_parameters.energy_interp_func_type(),
+                          model_parameters.conc_interp_func_type(), conc_db));
                }
             } else if (model_parameters.isConcentrationModelParabolic()) {
                if (model_parameters.norderp() > 1) {
@@ -264,6 +276,14 @@ class PhaseConcentrationsStrategyFactory
                              conc_a_scratch_id, conc_b_scratch_id,
                              model_parameters, conc_db));
                   }
+               } else {
+                  tbox::plog << "Parabolic..." << std::endl;
+                  assert(conc_b_scratch_id == -1);
+                  phase_conc_strategy.reset(
+                      new ParabolicEquilibriumPhaseConcentrationsBinary(
+                          conc_l_scratch_id, conc_a_scratch_id,
+                          model_parameters.energy_interp_func_type(),
+                          model_parameters.conc_interp_func_type(), conc_db));
                }
             }
          }
