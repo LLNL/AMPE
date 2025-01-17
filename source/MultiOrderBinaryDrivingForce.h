27ce539e15b975314e61c1a785f5ab6a34ff3a59
Jean-Luc Fattebert <fattebertj@ornl.gov>
2025-01-17 13:12:05 -0500

    New class MultiOrderBinaryDrivingForce (#295)
    * use it in QuadraticFreeEnergyMultiOrderBinary

---
+++ source/MultiOrderBinaryDrivingForce.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2018, Lawrence Livermore National Security, LLC and
+// UT-Battelle, LLC.
+// Produced at the Lawrence Livermore National Laboratory and
+// the Oak Ridge National Laboratory
+// LLNL-CODE-747500
+// All rights reserved.
+// This file is part of AMPE.
+// For details, see https://github.com/LLNL/AMPE
+// Please also read AMPE/LICENSE.
+//
+#ifndef MultiOrderBinaryDrivingForce_H
+#define MultiOrderBinaryDrivingForce_H
+
+#include "FreeEnergyStrategyBinary.h"
+
+#include "SAMRAI/hier/Patch.h"
+
+using namespace SAMRAI;
+
+class MultiOrderBinaryDrivingForce
+{
+ public:
+   MultiOrderBinaryDrivingForce(FreeEnergyStrategyBinary* fenergy_strategy);
+
+   void addDrivingForce(hier::Patch& patch, const int temperature_id,
+                        const int phase_id, const int conc_l_id,
+                        const int conc_a_id, const int f_l_id, const int f_a_id,
+                        const int rhs_id);
+
+ private:
+   FreeEnergyStrategyBinary* d_fenergy_strategy;
+};
+
+#endif
